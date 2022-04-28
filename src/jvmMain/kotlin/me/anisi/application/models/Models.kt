cd4c3e58831d0131ee6db41371548c3113afb07c
anisi <anisimov.prod@gmail.com>
2022-04-28 15:02:15 +0400

    add comments

--- src/jvmMain/kotlin/me/anisi/application/models/Models.kt
+++ src/jvmMain/kotlin/me/anisi/application/models/Models.kt
@@ -2,9 +2,24 @@ package me.anisi.application.models
 
 import java.time.LocalDateTime
 
+/**
+ * DTO адреса прокси
+ */
 data class ProxyAddressDTO(
+    /**
+     * Хост
+     */
     val host: String,
+    /**
+     * Порт
+     */
     val port: String,
+    /**
+     * Источник
+     */
     val source: String,
+    /**
+     * Дата создания
+     */
     val created: LocalDateTime
 )
